6ac48de848ce920f258b943b597e3c0e6521c378
Sam Ceracas <samceracas@gmail.com>
2020-12-13 13:46:18 +0800

    Updates

---
+++ precache-manifest.ab2b08fd4c583aaabb460e23235dc618.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edc1d444de631c06fde40692359b8446",
+    "url": "/json-pretty-print-online/index.html"
+  },
+  {
+    "revision": "321c1d22abfbfc3f5979",
+    "url": "/json-pretty-print-online/static/css/2.94f0b22b.chunk.css"
+  },
+  {
+    "revision": "57d15b2f8fbd1ea540b5",
+    "url": "/json-pretty-print-online/static/css/main.b34fb3b9.chunk.css"
+  },
+  {
+    "revision": "321c1d22abfbfc3f5979",
+    "url": "/json-pretty-print-online/static/js/2.e96352a3.chunk.js"
+  },
+  {
+    "revision": "a70c85f38a3096fa18f8264fe4c7cae3",
+    "url": "/json-pretty-print-online/static/js/2.e96352a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57d15b2f8fbd1ea540b5",
+    "url": "/json-pretty-print-online/static/js/main.ef2ca847.chunk.js"
+  },
+  {
+    "revision": "0f1d289de68aa0663700",
+    "url": "/json-pretty-print-online/static/js/runtime-main.f831f336.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4d689944a2dda16f2fd0c7b8d8026ff4.js"
+  "/json-pretty-print-online/precache-manifest.ab2b08fd4c583aaabb460e23235dc618.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/json-pretty-print-online/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
